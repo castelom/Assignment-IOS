8999dcd8304f6553c2fcd64640dbd913c015f1b4
Joao Victor Castelo <Victor@joaos-imac.local>
2016-05-05 00:32:09 +1000

    Settings screen working

--- Bubbles/GameController.m
+++ Bubbles/GameController.m
@@ -31,7 +31,23 @@
     [test setFrame:CGRectMake(test.position_x, test.position_y, widthBubble, heightBubble)];
     [self.view addSubview: test];
     NSLog(@"position %f %f", test.position_x, test.position_y);
-                                */
+    */
+}
+
+-(void)initView{
+    [super viewDidLoad];
+    self.score = 0;
+    self.remain_time = 10;
+    self.max_bubbles = 4;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(UpdateTime) userInfo:nil repeats:YES];
+    [self.timer fire];
+}
+
+/*Controller Methods*/
+-(void)ConfigureController{
+    [self registerNotifications];
+    self.label_score.text = [NSString stringWithFormat:@"Score    %d",self.score];
+    self.bubbles = [BubbleCollection CreateBubbles:self.max_bubbles withFrameX: self.view.frame.size.width withFrameY:self.view.frame.size.height];
 }
 
 -(void)UpdateTime{
@@ -46,23 +62,6 @@
     }
 }
 
--(void)initView{
-    [super viewDidLoad];
-    self.score = 0;
-    self.remain_time = 10;
-    self.max_bubbles = 4;
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(UpdateTime) userInfo:nil repeats:YES];
-    [self.timer fire];
-}
-
-
-
-/*Controller Methods*/
--(void)ConfigureController{
-    [self registerNotifications];
-    self.label_score.text = [NSString stringWithFormat:@"Score    %d",self.score];
-    self.bubbles = [BubbleCollection CreateBubbles:self.max_bubbles withFrameX: self.view.frame.size.width withFrameY:self.view.frame.size.height];
-}
 -(void) registerNotifications{
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification:) name:@"ADD_BUBBLES"    object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification:) name:@"REMOVE_BUBBLE" object:nil];
@@ -74,7 +73,7 @@
         [self addBubbles:notification.object];
     }
     else if([[notification name] isEqualToString:@"REMOVE_BUBBLE"]){
-        [self RemoveBubble:notification.object];
+        [self RemoveBubbleWithAnimation:notification.object];
     }
     
 }
@@ -98,7 +97,7 @@
     }
 }
 
--(void) RemoveBubble: (Bubble *) bubble{
+-(void) RemoveBubbleWithAnimation: (Bubble *) bubble{
     [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionCurveLinear
                      animations:^{
                          bubble.transform = CGAffineTransformScale(bubble.transform, 0.01, 0.01);
--- Bubbles/SettingsController.m
+++ Bubbles/SettingsController.m
@@ -9,19 +9,48 @@
 #import "SettingsController.h"
 
 @interface SettingsController ()
-
+@property (weak, nonatomic) IBOutlet UISegmentedControl *timer_options;
+@property (weak, nonatomic) IBOutlet UISlider *bubbles_option;
+@property (weak, nonatomic) IBOutlet UILabel *bubbles_label;
+@property (weak, nonatomic) IBOutlet UIButton *save_button;
 @end
 
 @implementation SettingsController
 
+
+
+int timer_segment = 0;
+int timer = 60;
+int num_bubbles = 15;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.bubbles_label.text = [@(num_bubbles) stringValue];
+    self.bubbles_option.value = (float) num_bubbles;
+    [self.timer_options setSelectedSegmentIndex:timer_segment];
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)SaveSettings:(id)sender {
+    //[self performSegueWithIdentifier:@"home_screen" sender:nil];
+    NSLog(@"time = %d, Max_bubbles = %d", timer, num_bubbles);
+}
+- (IBAction)ChooseTimer:(id)sender {
+    UISegmentedControl* timer_control = (UISegmentedControl*) sender;
+    timer_segment = (int)[timer_control selectedSegmentIndex];
+    NSString* string_timer = [timer_control titleForSegmentAtIndex:[timer_control selectedSegmentIndex]];
+    timer = (int)[string_timer integerValue];
+    
+}
+- (IBAction)ChooseBubbles:(id)sender {
+    UISlider* slider = (UISlider *) sender;
+    self.bubbles_label.text = [NSString stringWithFormat:@"%d", (int) slider.value];
+    num_bubbles = (int)[self.bubbles_label.text integerValue];
 }
 
 /*
